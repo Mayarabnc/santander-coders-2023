89dd96d9f3a9eb2e47aef28aec0d80332eb42975
Mayarabnc <mayarabncabral@gmail.com>
2023-09-18 20:03:20 -0300

    mudando a cor do fundo de acordo com o resultado

--- modulo 4/calculadora (exercicio de fixação 1)/script.js
+++ modulo 4/calculadora (exercicio de fixação 1)/script.js
@@ -8,12 +8,19 @@ let displayValue = '';
         function clearDisplay() {
             displayValue = '';
             document.getElementById('display').textContent = '0';
+            document.getElementById('display').style.backgroundColor = '#b6b6b6';
         }
 
         function calculateResult() {
             try {
                 displayValue = eval(displayValue);
                 document.getElementById('display').textContent = displayValue;
+        
+                if (displayValue >= 20) {
+                    document.getElementById('display').style.backgroundColor = 'green';
+                } else {
+                    document.getElementById('display').style.backgroundColor = 'orange'; 
+                }
             } catch (error) {
                 document.getElementById('display').textContent = 'Erro';
             }
